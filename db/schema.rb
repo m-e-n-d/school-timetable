12365e75fc9963a0d1def98c9319b4d5f9b97efe
aoking21 <ayky3305@gmail.com>
2020-09-27 13:06:08 +0900

    ログインフォームの内容を一部修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_042916) do
+ActiveRecord::Schema.define(version: 2020_09_06_042908) do
 
   create_table "periods", force: :cascade do |t|
     t.string "weekday"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_042916) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
